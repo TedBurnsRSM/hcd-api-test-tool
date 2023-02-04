855a02368c505e2d0301f227bd22487a3d1dcf7e
Christopher (Ted) Burns <ceburns111@gmail.com>
2023-02-04 02:14:04 -0500

    feat: generic forms submit yay

--- src/app/generic-form/generic-form.component.ts
+++ src/app/generic-form/generic-form.component.ts
@@ -15,11 +15,16 @@ export class GenericFormComponent extends ContentPage implements OnInit {
     {
       id: "ab889hf89", 
       name: "Contact Us",
-      fields: [["Last Name", "Ted"], ["First Name", "Burns"], ["Phone Number", "412-606-7377"], ["Comments", "I drove on a road today"]]},
-    {id: "ab897hgd9", 
-    name: "Weight Class Dispute",
-     fields: [["Dispute ID", "#43fda"], ["First Name", "Ted"], ["Last Name", "Burns"], ["Phone Number", "412-606-7377"], ["License No.", "NJd3fdsau"], ["Incident Date", "12/04/2021"]]}
+      fields: [
+        ["Last Name", "Ted"], 
+        ["First Name", "Burns"], 
+        ["Phone Number", "412-606-7377"], 
+        ["Comments", "I drove on a road today"]]},
   ];
+
+  public test: any 
+
+  public submitted: boolean = false;
 
   public formFields: [string, string][] = []; 
   
@@ -48,12 +53,13 @@ export class GenericFormComponent extends ContentPage implements OnInit {
     }
  }
 
-  onSubmit(values: string[]) {
-    console.log("submitted");
-    
-    
+  onSubmit() { 
+    this.submitted = true; 
+    console.log(this.formFields[0][0]);
+    console.log(this.formFields[1][0])
+
   }
-  
+
   }
 
   
